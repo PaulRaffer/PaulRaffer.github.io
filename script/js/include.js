7e2f64a931186a617dd176f0318b59c76e8980f0
Paul Raffer <znpivzcx.anonaddy.me>
2021-08-06 02:25:47 +0200

    Update file structure; ...

--- script/js/include.js
+++ script/js/include.js
@@ -3,15 +3,10 @@
 
 function loadFile(filePath)
 {
-	var result = null;
 	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.open("GET", filePath, true);
+	xmlhttp.open("GET", filePath, false);
 	xmlhttp.send();
-	if (xmlhttp.status==200) {
-		result = xmlhttp.responseText;
-	}
-	console.log(xmlhttp.status);
-	return result;
+	return xmlhttp.responseText;
 }
 
 
